0f4a31d1f070ebbd7d5ded4f14d03c1a5452cee8
Markus Baden <markus.baden@gmail.com>
2014-02-25 22:00:11 +0800

    Played around with hello world

--- python/yungui/hello_world.py
+++ python/yungui/hello_world.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
 import sys
 import matplotlib
 matplotlib.use('Qt4Agg')
-import pylab
+matplotlib.rcParams['backend.qt4'] = 'PySide'
 
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as \
+    FigureCanvas
 from matplotlib.figure import Figure
 
 from PySide import QtCore, QtGui
@@ -13,9 +13,14 @@ if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
 
     # generate the plot
-    fig = Figure(figsize=(600,600), dpi=72, facecolor=(1,1,1), edgecolor=(0,0,0))
+    fig = Figure(
+        figsize=(600, 600),
+        dpi=72,
+        facecolor=(1, 1, 1),
+        edgecolor=(0, 0, 0)
+    )
     ax = fig.add_subplot(111)
-    ax.plot([0,1])
+    ax.plot([0, 1])
     # generate the canvas to display the plot
     canvas = FigureCanvas(fig)
 
