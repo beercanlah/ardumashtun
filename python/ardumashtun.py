53b6c6da63dcb2dd95bb337b32efe801e4321e4a
Markus Baden <markus.baden@gmail.com>
2014-05-17 14:54:57 +0800

    Message numbers no longer hard coded

--- python/ardumashtun.py
+++ python/ardumashtun.py
@@ -1,5 +1,20 @@
 import serial
 
+kAcknowledge = 0
+kError = 1
+kFullStatus = 2
+kTemperature = 3
+kPumpStatus = 4
+kHeaterStatus = 5
+kDutyCycle = 6
+kPIDStatus = 7
+kPID = 8
+kHeater = 9
+kPump = 10
+kSetpoint = 11
+kPValue = 12
+kIValue = 13
+
 
 class UnoMashtun(object):
     '''
@@ -15,22 +30,26 @@ class UnoMashtun(object):
 
     @property
     def temperature(self):
-        self._request_value(3)
+        self._request_value(kTemperature)
         self._echo_readline()
 
     @property
     def pump(self):
-        self._request_value(4)
+        self._request_value(kPumpStatus)
         self._echo_readline()
 
     @pump.setter
     def pump(self, value):
-        self.serial.write('10,' + str(int(bool(value))) + ';\n\r')
+        self.serial.write(str(kPump) + ',' + str(int(bool(value))) + ';\n\r')
 
     @property
     def heater(self):
-        self._request_value(5)
+        self._request_value(kHeaterStatus)
         self._echo_readline()
+
+    @property
+    def dutycycle(self):
+        self._request_value
 
     def _open_port(self, port):
         ser = serial.Serial(port, self.baudrate, timeout=5)
