516280c38296d54a11f60b4adecbab4e461ec29f
Markus Baden <markus.baden@gmail.com>
2014-05-18 17:26:26 +0800

    Test for every message

--- python/test_ardumashtun.py
+++ python/test_ardumashtun.py
@@ -19,10 +19,64 @@ class TestMessages(unittest.TestCase):
         self.tun.pump
         self._assert_message_sent(4)
 
+    def test_heater_status(self):
+        self.tun.heater
+        self._assert_message_sent(5)
+
+    def test_dutycycle_status(self):
+        self.tun.dutycycle
+        self._assert_message_sent(6)
+
+    def test_pid_status(self):
+        self.tun.pid
+        self._assert_message_sent(7)
+
+    def test_setpoint_status(self):
+        self.tun.setpoint
+        self._assert_message_sent(8)
+
+    def test_p_value_status(self):
+        self.tun.p_value
+        self._assert_message_sent(9)
+
+    def test_i_value_status(self):
+        self.tun.i_value
+        self._assert_message_sent(10)
+
+    def test_pid(self):
+        self.tun.pid = 1
+        self._assert_message_sent_with_value(11, '1')
+
+    def test_dutycycle(self):
+        self.tun.dutycycle = 100
+        self._assert_message_sent_with_value(12, '100')
+
+    def test_pump(self):
+        self.tun.pump = 1
+        self._assert_message_sent_with_value(13, '1')
+
+    def test_setpoint(self):
+        self.tun.setpoint = 55.0
+        self._assert_message_sent_with_value(14, '55.0')
+
+    def test_p_value(self):
+        self.tun.p_value = 10.0
+        self._assert_message_sent_with_value(15, '10.0')
+
+    def test_i_value(self):
+        self.tun.i_value = 0.15
+        self._assert_message_sent_with_value(16, '0.15')
+
     def _assert_message_sent(self, msg_no):
         # I need to call this with () because I patched the
         # class and not the instance
         self.mock_serial().write.assert_called_with(str(msg_no) + ';\n\r')
+
+    def _assert_message_sent_with_value(self, msg_no, value):
+        # I need to call this with () because I patched the
+        # class and not the instance
+        msg = str(msg_no) + ',' + str(value) + ';\n\r'
+        self.mock_serial().write.assert_called_with(msg)
 
 if __name__ == '__main__':
     unittest.main()
