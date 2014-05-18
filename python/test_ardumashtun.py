ef1985d9c070ffdb1557c333dcd845d5dd7f27f5
Markus Baden <markus.baden@gmail.com>
2014-05-18 16:12:47 +0800

    First test failes successfully

--- python/test_ardumashtun.py
+++ python/test_ardumashtun.py
@@ -1,8 +1,16 @@
 import unittest
+import mock
+import ardumashtun
 
 
 class TestMessages(unittest.TestCase):
-    pass
+
+    @mock.patch('ardumashtun.serial.Serial')
+    def test_temperature(self, mock_serial):
+        tun = ardumashtun.UnoMashtun('')
+        tun.temperature
+
+        mock_serial.write.assert_called_with('1;\n\r')
 
 if __name__ == '__main__':
     unittest.main()
